950bb7411deca32fb58862283e2490b7698ba333
leocoder <singhra66@gmail.com>
2023-11-03 13:59:08 +0530

    Added: Base RN with Nativewind

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text className="text-2xl text-purple-300">Hello this is Test</Text>
+    </View>
+  );
+}
+
+export default App;
